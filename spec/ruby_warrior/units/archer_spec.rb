6834f6c1d59ba418661e67602efa1cb673a6b85a
Patricio Bruna <pbruna@gmail.com>
2012-04-30 15:42:17 -0400

    Modify spec require to work with ruby 1.9.3

--- spec/ruby_warrior/units/archer_spec.rb
+++ spec/ruby_warrior/units/archer_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require "./" + File.dirname(__FILE__) + '/../../spec_helper'
 
 describe RubyWarrior::Units::Archer do
   before(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+#require 'rubygems'
 require 'spec'
 require File.dirname(__FILE__) + '/../lib/ruby_warrior'
 
